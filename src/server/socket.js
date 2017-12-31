ba06e49800ac27382f4dcf0d55d5e645731a340d
Izra Faturrahman <Frizz925@hotmail.com>
2017-12-31 17:34:54 +0700

    Added socket debugging option

--- src/server/socket.js
+++ src/server/socket.js
@@ -1,9 +1,14 @@
+const registerDebug = (server, hooks) => {
+  hooks.forEach((hook) => {
+    server.on("socket." + hook, ({data}) => {
+      server.logger.debug(hook, data);
+    });
+  });
+};
+
 export default function socket() {
-  this.on("socket.actionSuccess", ({data}) => {
-    this.logger.debug("actionSuccess", data);
-  });
-  this.on("socket.actionFail", ({data}) => {
-    this.logger.debug("actionFail", data);
-  });
+  if (this.config.Log.DebugSocket) {
+    registerDebug(this, ["actionSuccess", "actionFail", "sendAction"]);
+  }
   return {};
 }
