3777f2a3ec8d3966c075106e4cc4bd7244b0ecb2
Izra Faturrahman <Frizz925@hotmail.com>
2018-01-21 08:45:49 +0700

    Make Location.Change automatically converts location into string

--- src/server/steps/Location/Change.js
+++ src/server/steps/Location/Change.js
@@ -1,6 +1,8 @@
 import Step from "../Step";
+import {locationToString} from "~/helpers/LocationHelper";
 
 exports = module.exports = (logger, worker) => (url) => {
+  url = locationToString(url);
   return Step("Location", async function Change() {
     logger.debug("Location change:", url);
     return await worker.sendAction("location.change", url);
