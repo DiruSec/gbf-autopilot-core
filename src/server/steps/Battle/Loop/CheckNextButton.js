9c9cb72dfa86f38fd192b995282c4ca76915d468
Izra Faturrahman <Frizz925@hotmail.com>
2018-02-12 23:58:19 +0700

    Improved in-battle checking behavior

--- src/server/steps/Battle/Loop/CheckNextButton.js
+++ src/server/steps/Battle/Loop/CheckNextButton.js
@@ -1,5 +1,5 @@
 import Step from "../../Step";
-import {enemyAlive} from "~/helpers/StateHelper";
+import { enemyAlive } from "~/helpers/StateHelper";
 
 exports = module.exports = (require, run, process) => (next, stop) => {
   const Wait = require("steps/Wait");
@@ -9,57 +9,75 @@ exports = module.exports = (require, run, process) => (next, stop) => {
   const ClickNextButton = require("steps/Battle/Loop/ClickNextButton");
   const CheckNextLocation = require("steps/Battle/Loop/CheckNextLocation");
 
-  const waitForLocationOrNextButton = () => new Promise((resolve, reject) => {
-    // We have 2 checkers running in parallel: location and DOM.
-    // If the location checker gets triggered first, 
-    // tells the DOM checker to ignore any resolved promise.
-    var waiting = true;
-    process([
-      Location.Wait(),
-      CheckNextLocation(),
-      async (_, inBattle) => {
-        waiting = false;
-        if (!inBattle) return {inBattle};
-        await run(Wait(".btn-attack-start.display-on,.btn-result,.cnt-result"));
-        try {
-          await run(Check(".btn-result"));
-          return {inBattle, hasNextButton: true};
-        } catch (e) {
-          return {inBattle, hasNextButton: false};
-        }
-      }
-    ]).then(resolve, reject);
+  const nextLoopSelector = [
+    ".btn-attack-start.display-on",
+    ".btn-result",
+    ".cnt-result"
+  ].join(",");
 
-    run(Wait(".btn-result")).then(() => {
-      if (!waiting) return;
-      resolve({inBattle: true, hasNextButton: true});
+  const waitForLocationOrNextButton = () =>
+    new Promise((resolve, reject) => {
+      // We have 2 checkers running in parallel: location and DOM.
+      // If the location checker gets triggered first,
+      // tells the DOM checker to ignore any resolved promise.
+      var waiting = true;
+      process([
+        Location.Wait(),
+        CheckNextLocation(),
+        async (_, inBattle) => {
+          waiting = false;
+          if (!inBattle) return { inBattle };
+          await run(Wait(nextLoopSelector));
+          try {
+            await run(Check(".btn-result"));
+            return { inBattle, hasNextButton: true };
+          } catch (e) {
+            return { inBattle, hasNextButton: false };
+          }
+        }
+      ]).then(resolve, reject);
+
+      run(Wait(".btn-result")).then(() => {
+        if (!waiting) return;
+        resolve({ inBattle: true, hasNextButton: true });
+      });
     });
-  });
 
   return Step(function checkNextButton() {
-    return run(State()).then((state) => {
-      // check if there's still enemy left
-      // if true, check for the next button
-      // else, wait for the next button to appear (expected usual behavior)
-      // EDIT: in cases of refreshes, it may instead redirect to the next battle or result page
-      if (enemyAlive(state)) {
-        return run(Check(".btn-result")).catch(() => ({
-          hasNextButton: true,
-          inBattle: true
-        }), () => ({
-          hasNextButton: false,
-          inBattle: true
-        }));
-      } else {
-        return waitForLocationOrNextButton();
-      }
-    }).then((condition) => {
-      if (condition.inBattle) {
-        return condition.hasNextButton ? run(ClickNextButton()).then(stop) : next();
-      } else {
-        return stop(false);
-      }
-    });
+    return run(State())
+      .then(state => {
+        // check if there's still enemy left
+        // if true, check for the next button
+        // else, wait for the next button to appear (expected usual behavior)
+        // EDIT: in cases of refreshes, it may instead redirect to the next battle or result page
+        if (enemyAlive(state)) {
+          return run(Wait(nextLoopSelector))
+            .then(() => {
+              return run(Check(".btn-result"));
+            })
+            .then(
+              () => ({
+                hasNextButton: true,
+                inBattle: true
+              }),
+              () => ({
+                hasNextButton: false,
+                inBattle: true
+              })
+            );
+        } else {
+          return waitForLocationOrNextButton();
+        }
+      })
+      .then(condition => {
+        if (condition.inBattle) {
+          return condition.hasNextButton
+            ? run(ClickNextButton()).then(stop)
+            : next();
+        } else {
+          return stop(false);
+        }
+      });
   });
 };
 
